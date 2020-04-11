ca5d42a6d012e663ba179ec113489ba6ed2091a2
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-04-11 12:03:19 +0900

    added new test method for URL Get and Post requests.

--- bookinghub/tests/test_urls.py
+++ bookinghub/tests/test_urls.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
+from django.test import Client
 from bookinghub.views import user_login
 
 
@@ -12,3 +13,12 @@ class TestUrls(SimpleTestCase):
     def test_logout_url_is_resolved(self):
         url = reverse('logout')
         self.assertEquals(resolve(url).func, user_login)
+
+# static method should be called in a static context for testing
+    def print_status_code(self):
+        c = Client()
+        response = c.post('/login', {'username': 'EECS393', 'password': 'EECS393'})
+        print(response.status_code)
+        response = c.get('/reservation')
+        response.content
+
