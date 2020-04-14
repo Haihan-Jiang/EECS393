37175a4fbae86bb20de45b16e744f7978c1ce7fd
Walter Nam <49884140+wnam98@users.noreply.github.com>
2020-04-14 17:35:46 +0900

    made new unit tests for test_views and test_urls.py To run the tests, run python manage.py test bookinghub in your terminal

--- bookinghub/tests/test_urls.py
+++ bookinghub/tests/test_urls.py
@@ -1,24 +1,38 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from django.test import Client
-from bookinghub.views import user_login
+from bookinghub.views import post_list, user_login, dashboard, reservation, confirmation, register
 
-'''
-class TestUrls(SimpleTestCase):
 
+class TestURLS(SimpleTestCase):
+
+    '''
     def test_login_url_is_resolved(self):
         url = reverse('login')
-        self.assertEquals(resolve(url).func, user_login)
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, LoginView)
 
     def test_logout_url_is_resolved(self):
         url = reverse('logout')
-        self.assertEquals(resolve(url).func, user_login)
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, LogoutView)
+    '''
 
-# static method should be called in a static context for testing
-    def print_status_code(self):
-        c = Client()
-        response = c.post('/login', {'username': 'EECS393', 'password': 'EECS393'})
-        print(response.status_code)
-        response = c.get('/reservation')
-        response.content
-      '''
+    def test_dashboard_url_is_resolved(self):
+        url = reverse('dashboard')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, dashboard)
+
+    def test_reservation_url_is_resolved(self):
+        url = reverse('reservation')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, reservation)
+
+    def test_confirmation_url_is_resolved(self):
+        url = reverse('confirmation')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, confirmation)
+
+    def test_register_url_is_resolved(self):
+        url = reverse('register')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, register)
--- bookinghub/tests/test_views.py
+++ bookinghub/tests/test_views.py
@@ -1,2 +1,37 @@
 # views (uses reverse)
+from django.test import TestCase, Client
+from django.urls import reverse
+from bookinghub.models import PublishedManager, Post, User, reservation, room, hotel, hotelStaff
+import json
 
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.list_url = reverse('list')
+        self.login_url = reverse('login')
+
+    def test_post_list_GET(self):
+        client = Client()
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'bookinghub/post/list.html')
+
+    def test_user_login(self):
+        client = Client()
+        response = self.client.get(self.login_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/login.html')
+
+    def test_dashboard(self):
+        pass
+
+    def test_reservation(self):
+        pass
+
+    def test_confirmation(self):
+        pass
+
+    def test_register(self):
+        pass
